ccfa2e210924b57adb5386934b7f97905143e6ea
Emeka <frankeokafor@gmail.com>
2026-02-28 20:35:18 +0100

    Added some little smart layout features. Just practicing things like conditional tags like is_home(), is_front_page() and more

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 </body>
 <footer>
+  <?php if(is_single()): ?>
+    <aside class="sidebar">
+      <p>This is a blog sidebar.</p>
+    </aside>
+  <?php endif; ?>
   <?php wp_footer(); ?>
 </footer>
 </html>
