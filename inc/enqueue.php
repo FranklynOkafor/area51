de9839952e44809f0b16191f63f81e2fa153afa5
Emeka <frankeokafor@gmail.com>
2026-02-28 22:24:18 +0100

    adding some styling to the banner

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,11 +1,18 @@
 <?php
 
-function ulo_enqueue_assets() {
+function ulo_enqueue_assets()
+{
     wp_enqueue_style(
         'ulo-main-style',
         get_template_directory_uri() . '/assets/css/main.css',
         [],
         '1.0'
+    );
+    // Header Style
+    wp_enqueue_style(
+        'ulo-header-style',
+        get_template_directory_uri() . '/assets/css/header.css',
+        
     );
 
     wp_enqueue_script(
